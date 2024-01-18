92dbd99234ae5136764455b9dbced3ab61732ae6
Ekrem05 <ekremnptg@gmail.com>
2024-01-18 22:06:20 +0200

    Enough for today

--- HoneyZoneMvc/HoneyZoneMvc/obj/Debug/net6.0/HoneyZoneMvc.AssemblyInfo.cs
+++ HoneyZoneMvc/HoneyZoneMvc/obj/Debug/net6.0/HoneyZoneMvc.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoneyZoneMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+866beb4b54c1402f8f6384ed25f896941dbf6b6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoneyZoneMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoneyZoneMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
