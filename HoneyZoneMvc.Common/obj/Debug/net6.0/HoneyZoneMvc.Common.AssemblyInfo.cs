840448f3adf0e8f715eb786a4528660f2bca1e5c
Ekrem05 <ekremnptg@gmail.com>
2024-03-03 21:23:07 +0200

    Hide connection string

--- HoneyZoneMvc.Common/obj/Debug/net6.0/HoneyZoneMvc.Common.AssemblyInfo.cs
+++ HoneyZoneMvc.Common/obj/Debug/net6.0/HoneyZoneMvc.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoneyZoneMvc.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4eaad1998b1d6dfba72a6834d735506b8563d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905c10bd36cebd607b48f9987743c053fe37121c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoneyZoneMvc.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoneyZoneMvc.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HoneyZoneMvc.Infrastructure/obj/Debug/net6.0/HoneyZoneMvc.Infrastructure.AssemblyInfo.cs
+++ HoneyZoneMvc.Infrastructure/obj/Debug/net6.0/HoneyZoneMvc.Infrastructure.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoneyZoneMvc.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4eaad1998b1d6dfba72a6834d735506b8563d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905c10bd36cebd607b48f9987743c053fe37121c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoneyZoneMvc.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoneyZoneMvc.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
