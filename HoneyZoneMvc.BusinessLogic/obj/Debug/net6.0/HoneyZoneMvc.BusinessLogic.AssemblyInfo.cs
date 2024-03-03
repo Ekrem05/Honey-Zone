905c10bd36cebd607b48f9987743c053fe37121c
Ekrem05 <ekremnptg@gmail.com>
2024-03-03 20:29:43 +0200

    HoneyZoneMvc.Common added for static variables

--- HoneyZoneMvc.BusinessLogic/obj/Debug/net6.0/HoneyZoneMvc.BusinessLogic.AssemblyInfo.cs
+++ HoneyZoneMvc.BusinessLogic/obj/Debug/net6.0/HoneyZoneMvc.BusinessLogic.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoneyZoneMvc.BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c0f44579e28cac628b796645de95485a608748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4eaad1998b1d6dfba72a6834d735506b8563d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoneyZoneMvc.BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoneyZoneMvc.BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
