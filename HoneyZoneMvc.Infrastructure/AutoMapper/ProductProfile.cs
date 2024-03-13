c6c8751964279a09a0b0923cf3e59c92e8f5d644
Ekrem05 <ekremnptg@gmail.com>
2024-03-13 11:34:02 +0200

    Added mapping configurations

--- HoneyZoneMvc.Infrastructure/AutoMapper/ProductProfile.cs
+++ HoneyZoneMvc.Infrastructure/AutoMapper/ProductProfile.cs
@@ -12,8 +12,20 @@ namespace HoneyZoneMvc.Infrastructure.AutoMapper
             CreateMap<ProductAdminViewModel, ProductShopCardViewModel>()
              .ForMember(dest => dest.IsAvailable, opt => opt.MapFrom(src => src.QuantityInStock > 0));
 
+            CreateMap<ProductAdminViewModel, ProductEditViewModel>()
+                .ForMember(m => m.Categories, map => map.Ignore());
+
+
+            CreateMap<ProductAddViewModel, Product>()
+                    .ForMember(m => m.Images, map => map.Ignore())
+                    .ForMember(m => m.MainImageUrl, map => map.Ignore())
+                    .ForMember(m => m.CategoryId, map => map.MapFrom(vm => Guid.Parse(vm.CategoryId)));
+
             CreateMap<Product, ProductAdminViewModel>()
-                    .ForMember(m=>m.Images,map=>map.MapFrom(vm=>vm.Images.Select(x=>x.Name)));
+                   .ForMember(vm => vm.Id, map => map.MapFrom(m => m.Id.ToString()))
+                   .ForMember(vm => vm.MainImageName, map => map.MapFrom(m => m.MainImageUrl))
+                   .ForMember(vm => vm.CategoryId, map =>map.MapFrom(m=>m.Category.Name))
+                   .ForMember(vm => vm.Images, map => map.MapFrom(m => m.Images.Select(i => i.Name).ToArray()));
         }
     }
 }
