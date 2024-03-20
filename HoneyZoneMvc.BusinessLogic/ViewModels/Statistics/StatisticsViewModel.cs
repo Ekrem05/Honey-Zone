44ac455ae3cc39ee8b9d033829983cb013db6e5e
Ekrem05 <ekremnptg@gmail.com>
2024-03-20 18:36:08 +0200

    UI for chart fully working

---
+++ HoneyZoneMvc.BusinessLogic/ViewModels/Statistics/StatisticsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyZoneMvc.BusinessLogic.ViewModels.Statistics
+{
+    public class StatisticsViewModel
+    {
+        public CategoryStatisticsViewModel CategoryStatistic { get; set; } = new CategoryStatisticsViewModel();
+    }
+}
--- HoneyZoneMvc.BusinessLogic/obj/Debug/net6.0/HoneyZoneMvc.BusinessLogic.AssemblyInfo.cs
+++ HoneyZoneMvc.BusinessLogic/obj/Debug/net6.0/HoneyZoneMvc.BusinessLogic.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoneyZoneMvc.BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f390dd44ed9d880d133e06afc44d2cbb01344710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe44639e287a7d370e93172656ea15ec4391dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoneyZoneMvc.BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoneyZoneMvc.BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
