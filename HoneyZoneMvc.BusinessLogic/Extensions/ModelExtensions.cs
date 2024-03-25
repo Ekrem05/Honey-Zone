28fd5599f2c33a79fcf9ca7368118d2e7d6a247c
Ekrem05 <ekremnptg@gmail.com>
2024-03-25 08:42:42 +0200

    Added Extension methods for CategoryStatistic Model

---
+++ HoneyZoneMvc.BusinessLogic/Extensions/ModelExtensions.cs
@@ -0,0 +1,29 @@
+﻿using HoneyZoneMvc.BusinessLogic.Contracts.ExtensionContracts;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyZoneMvc.BusinessLogic.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static string GetCategoryLabels(this ICategoryStatistic categoryStatistic)
+        {
+            return string.Join(", ", categoryStatistic.Categories.Select(x => $"'{x}'"));
+        }
+        public static string GetCategoryData(this ICategoryStatistic categoryStatistic)
+        { 
+           List<string> values = new();
+            
+           foreach(var categoryName in categoryStatistic.Categories)
+           {
+                values.Add(categoryStatistic.ProductsSoldInCategory[categoryName].ToString());
+           }
+            return string.Join(", ", values);
+        }
+    }
+}
--- HoneyZoneMvc.BusinessLogic/ViewModels/Statistics/CategoryStatisticsViewModel.cs
+++ HoneyZoneMvc.BusinessLogic/ViewModels/Statistics/CategoryStatisticsViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HoneyZoneMvc.BusinessLogic.Contracts.ExtensionContracts;
 
 namespace HoneyZoneMvc.BusinessLogic.ViewModels.Statistics
 {
-    public class CategoryStatisticsViewModel
+    public class CategoryStatisticsViewModel:ICategoryStatistic
     {
         public string[] Categories { get; set; }
         public Dictionary<string, int> ProductsSoldInCategory { get; set; }
