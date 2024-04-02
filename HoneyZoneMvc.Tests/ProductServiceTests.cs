5c4d86135b7ef729fb88f0bccb0821275821213f
Ekrem05 <ekremnptg@gmail.com>
2024-04-02 12:23:14 +0300

    OrderService tested

--- HoneyZoneMvc.Tests/ProductServiceTests.cs
+++ HoneyZoneMvc.Tests/ProductServiceTests.cs
@@ -13,6 +13,9 @@ using System.Text;
 
 namespace HoneyZoneMvc.Tests
 {
+    /// <summary>
+    /// This class contains tests for the ProductService class.
+    /// </summary>
     public class ProductServiceTests
     {
         private DbContextOptions<ApplicationDbContext> dbOptions;
@@ -55,7 +58,7 @@ namespace HoneyZoneMvc.Tests
         }
 
         [Test]
-        public  async Task AddProductAsync_Successfully_Test()
+        public async Task AddProductAsync_Successfully_Test()
         {
          
             var imageMockOne = new Mock<IFormFile>();
@@ -82,7 +85,6 @@ namespace HoneyZoneMvc.Tests
             Assert.That(dbContext.Products.Count()==8, "Adding product does not work");
 
         }
-
         [Test]
         public void AddProductAsync_ThrowsException_WhenNull()
         {
@@ -212,6 +214,84 @@ namespace HoneyZoneMvc.Tests
         public void DeleteAsync_ThrowsException()
         {
             Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.DeleteAsync("Invalid ID"));
+        }
+        [Test]
+        public async Task SetDiscountAsync_WorksCorrectly()
+        {
+            ProductDiscountViewModel vm= new ProductDiscountViewModel
+            {
+                Id = "c7ecd019-40b1-47f3-89c4-67e3625f796b",
+                Discount = 10
+            };
+
+            await productService.SetDiscountAsync(vm);
+            var proudct = dbContext.Products.FirstOrDefault(p => p.Id == Guid.Parse("c7ecd019-40b1-47f3-89c4-67e3625f796b"));
+            Assert.That(proudct.Discount == 10&& proudct.IsDiscounted==true, "SetDiscountAsync does not work");
+        }
+        [Test]
+        public void SetDiscountAsync_ThrowsException()
+        {
+            ProductDiscountViewModel vm = new ProductDiscountViewModel
+            {
+                Id = "INVALID ID",
+                Discount = 10
+            };     
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.SetDiscountAsync(vm));
+            
+        }
+        [Test]
+        public async Task SetDiscountByCategoryAsync_WorksCorrectly()
+        {
+            await productService.SetDiscountByCategoryAsync("78355d47-6040-4676-9972-ac8be4f19882", 10);
+            var products = dbContext.Products.Where(p => p.CategoryId == Guid.Parse("78355d47-6040-4676-9972-ac8be4f19882")).ToList();
+            Assert.That(products.All(p => p.Discount == 10 && p.IsDiscounted == true), "SetDiscountByCategoryAsync does not work");
+            
+        }
+        [Test]
+        public void SetDiscountByCategoryAsync_ThrowsException()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.SetDiscountByCategoryAsync(null,10),"SetDiscountByCategoryAsync doesnt throw exception when id is null");
+        }
+        [Test]
+        public async Task RemoveDiscountAsync_WorksCorrectly()
+        {
+            await productService.RemoveDiscountAsync("c7ecd019-40b1-47f3-89c4-67e3625f796b");
+            var product = dbContext.Products.FirstOrDefault(p => p.Id == Guid.Parse("c7ecd019-40b1-47f3-89c4-67e3625f796b"));
+            Assert.That(product.Discount == 0 && product.IsDiscounted == false, "RemoveDiscountAsync does not work");
+
+        }
+        [Test]
+        public void  RemoveDiscountAsync_ThrowsException()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.RemoveDiscountAsync("Invalid ID"));
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.RemoveDiscountAsync(null));
+
+        }
+        [Test]
+        public async Task DecreaseQuantityAsync_WorksCorrectly()
+        {
+            await productService.DecreaseQuantityAsync("c7ecd019-40b1-47f3-89c4-67e3625f796b");
+            var product = dbContext.Products.FirstOrDefault(p => p.Id == Guid.Parse("c7ecd019-40b1-47f3-89c4-67e3625f796b"));
+            Assert.That(product.QuantityInStock == 81, "DecreaseQuantityAsync does not work");
+        }
+        [Test]
+        public async Task IncreaseTotalOrdersAsync_WorksCorrectly()
+        {
+            await productService.IncreaseTotalOrdersAsync("c7ecd019-40b1-47f3-89c4-67e3625f796b",3);
+            var product = dbContext.Products.FirstOrDefault(p => p.Id == Guid.Parse("c7ecd019-40b1-47f3-89c4-67e3625f796b"));
+            Assert.That(product.TimesOrdered == 4, "IncreaseTotalOrdersAsync does not work");
+        }
+        [Test]
+        public void IncreaseTotalOrdersAsync_ThrowsException()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.IncreaseTotalOrdersAsync("Invalid ID", 3));
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await productService.IncreaseTotalOrdersAsync(null, 3));
+        }
+        [Test]
+        public async Task SearchAsync_WorksCorrectly()
+        {
+            var vm = await productService.SearchAsync("HoNeY");
+            Assert.That(vm.Count() == 3, "SearchAsync does not work");
         }
         [TearDown]
         public void CleanUp()
