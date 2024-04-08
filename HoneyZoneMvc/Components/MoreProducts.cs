ffcdaea5aa337254a2eb36a566462c78bf59d0d4
Ekrem05 <ekremnptg@gmail.com>
2024-04-08 13:06:20 +0300

    ViewComponent Added

---
+++ HoneyZoneMvc/Components/MoreProducts.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HoneyZoneMvc.BusinessLogic.Contracts.ServiceContracts;
+using HoneyZoneMvc.BusinessLogic.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoneyZoneMvc.Components
+{
+    public class MoreProducts:ViewComponent
+    {
+        private IProductService productService;
+        private IMapper mapper;
+        public MoreProducts(IProductService _productService, IMapper _mapper)
+        {
+            productService= _productService;
+            mapper= _mapper;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = await productService.AllAsync();
+            var result= mapper.Map<List<ProductShopCardViewModel>>(products);
+            return View(result);
+        }
+    }
+}
